5b944ea33b6139e196156602963f116efe448b96
markflow <sebastianimfeld@gmail.com>
2019-06-10 11:08:31 -0300

    upgrade

--- tasks-app/app.js
+++ tasks-app/app.js
@@ -1,6 +1,8 @@
 $(document).ready( function() {
     console.log("jquery is working");
 
+    let edit = false;
+
     $('#task-result').hide();
 
     fetchTasks();
@@ -35,9 +37,13 @@ $(document).ready( function() {
     $('#task-form').submit(function(e) {
         const postData = {
             name: $('#name').val(),
-            description: $('#description').val()
+            description: $('#description').val(),
+            id: $('#taskId').val()
         };
-        $.post('task-add.php', postData, function (response) {
+
+        let url = edit === false ? 'task-add.php' : 'task-edit.php';
+        console.log(url);
+        $.post(url, postData, function (response) {
             fetchTasks();
             console.log(response);
 
@@ -58,7 +64,7 @@ $(document).ready( function() {
                     template += `
                         <tr taskId="${task.id}">
                             <td>${task.id}</td>
-                            <td>${task.name}</td>
+                            <td><a href="#" class="task-item">${task.name}</a></td>
                             <td>${task.description}</td>
                             <td>
                                 <button class="task-delete btn btn-danger">
@@ -81,6 +87,19 @@ $(document).ready( function() {
                 fetchTasks();
             })
         }
+    })
+
+
+    $(document).on('click', '.task-item', function() {
+        let element = $(this)[0].parentElement.parentElement;
+        let id = $(element).attr('taskId');
+        $.post('task-single.php', {id}, function(response) {
+            const task = JSON.parse(response)
+            $('#taskId').val(task.id);
+            $('#name').val(task.name);
+            $('#description').val(task.description);
+            edit = true;
+        })
     })
 
 });
