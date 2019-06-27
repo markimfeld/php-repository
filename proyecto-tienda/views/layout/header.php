5fed6a648f75d7b3748964fc4a8e2349c1e8f2f2
marcos <sebastianimfeld@gmail.com>
2019-06-26 23:33:42 -0300

    upgrade

--- proyecto-tienda/views/layout/header.php
+++ proyecto-tienda/views/layout/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <title>Tienda de Camisetas</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="<?= BASE_URL?>assets/css/style.css"/>
     </head>
     <body>
